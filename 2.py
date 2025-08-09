f5e338c61bd5feeda7b1d86e32c47b13d52a733b
Shivambegineerpythondev <mrjsanju@gmail.com>
2025-08-09 19:03:16 +0530

    second

--- 2.py
+++ 2.py
@@ -1,2 +1,6 @@
-import keyword
-print(keyword.kwlist)
+print("DOB OF ME MY SISTER AND MY PARENTS")
+a = "17/11/2016"
+b = "25/05/2007"
+c = "09/01/1971"
+d = "06/11/1978"
+print(a,"\n",b,"\n",c,"\n",d,"\n")
